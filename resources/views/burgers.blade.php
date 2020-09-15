298a8bac05614466c6e0f0cfc128ccfc1c139787
ramesh dahal <rameshdahal2013@gmail.com>
2020-09-15 11:19:01 -0400

    first commit

---
+++ resources/views/burgers.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Burgers</title>
+</head>
+<body>
+    <div>
+        <h1>This is the burgers page</h1>
+    </div>
+    
+</body>
+</html>
